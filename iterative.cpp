7fbf8dd902f9eefebc313625c52c2424501b179b
martincrb <martincristobaltdr@gmail.com>
2020-06-20 18:49:44 +0200

    feat: initial commit

---
+++ iterative.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int binarySearchRecursive(int array[], int left, int right, int objective) { 
+    while(left <= right) {
+        int mid = left + (right - 1) / 2;
+        if (array[mid] == objective) return mid;
+        if (array[mid] < objective) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+int main() {
+    int array[] = {1, 5, 10, 43, 101, 202};
+    int objective = 101;
+    int sizeArray = 6; // sizeof(array) / sizeof(array[0])
+    int result = binarySearchRecursive(array, 0, sizeArray - 1, objective);
+    if (result == -1) {
+        cout << "El elemento no se ha encontrado" << endl;
+    } else {
+        cout << "El Elemento se ha encontrado" << result << endl;
+    }
+}
---
+++ recursive.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int binarySearchRecursive(int array[], int left, int right, int objective) { 
+    if (right >= left) {
+        int mid = left + (right - 1) / 2;
+        if (array[mid] == objective) {
+            return mid;
+        }
+        if (array[mid] > objective) {
+            return binarySearchRecursive(array, left, mid - 1, objective);
+        }
+        return binarySearchRecursive(array, mid + 1, right, objective);
+    } 
+    return -1;
+}
+int main() {
+    int array[] = {1, 5, 10, 43, 101, 202};
+    int objective = 102;
+    int sizeArray = 6; // sizeof(array) / sizeof(array[0])
+    int result = binarySearchRecursive(array, 0, sizeArray - 1, objective);
+    if (result == -1) {
+        cout << "El elemento no se ha encontrado" << endl;
+    } else {
+        cout << "El Elemento se ha encontrado" << result << endl;
+    }
+}
