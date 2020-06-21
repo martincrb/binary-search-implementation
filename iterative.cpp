5e21f57ae8c228f51a636af29c8cf4c9dcb6d4ba
martincrb <martincristobaltdr@gmail.com>
2020-06-21 11:15:34 +0200

    fix: name of func

--- iterative.cpp
+++ iterative.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int binarySearchRecursive(int array[], int left, int right, int objective) { 
+int binarySearchIterative(int array[], int left, int right, int objective) { 
     while(left <= right) {
         int mid = left + (right - left) / 2;
         if (array[mid] == objective) return mid;
@@ -14,7 +14,7 @@ int main() {
     int array[] = {1, 5, 10, 43, 101, 202};
     int objective = 101;
     int sizeArray = 6; // sizeof(array) / sizeof(array[0])
-    int result = binarySearchRecursive(array, 0, sizeArray - 1, objective);
+    int result = binarySearchIterative(array, 0, sizeArray - 1, objective);
     if (result == -1) {
         cout << "El elemento no se ha encontrado" << endl;
     } else {
